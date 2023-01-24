bd89b2f434861ef14a6a1de584400609f7e845f9
Gabriel Oliveira <gabrielsantosrv@outlook.com>
2023-01-24 13:47:40 -0300

    fixing image transformations

--- clip_pt/src/utils/dataset/custom_dataset.py
+++ clip_pt/src/utils/dataset/custom_dataset.py
@@ -1,16 +1,11 @@
-import os
 import json
+import os
 from collections import defaultdict
 
 import pandas as pd
 import torch
 from PIL import Image
 from torchvision import transforms
-from torchvision.transforms import InterpolationMode
-
-
-def _convert_image_to_rgb(image):
-    return image.convert("RGB")
 
 
 class CustomDataset(torch.utils.data.Dataset):
@@ -27,15 +22,7 @@ class CustomDataset(torch.utils.data.Dataset):
         self.dataset_name = dataset_name
         self.image_base_dir = image_base_dir
 
-        # same size used in original CLIP
-        self.transform = transforms.Compose([
-            transforms.Resize(256, interpolation=InterpolationMode.BICUBIC),
-            transforms.CenterCrop(224),
-            _convert_image_to_rgb,
-            transforms.ToTensor(),
-            transforms.Normalize((0.48145466, 0.4578275, 0.40821073),
-                                 (0.26862954, 0.26130258, 0.27577711)),
-        ])
+        self.transform = transforms.ToTensor()
 
         if dataset_name.lower() == "pracegover":
             self.dataset = self.__read_pracegover(dataset_path, split)
