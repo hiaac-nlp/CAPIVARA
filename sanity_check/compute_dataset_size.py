4949f2eeba6e70af82679ba4f470f9b57598b04f
Gabriel Oliveira <gabrielsantosrv@outlook.com>
2023-02-16 15:41:14 -0300

    computing dataset size

--- sanity_check/compute_dataset_size.py
+++ sanity_check/compute_dataset_size.py
@@ -1,25 +1,18 @@
 import argparse
+import tarfile
 
 import braceexpand
-import webdataset as wds
 import tqdm
-from tqdm.contrib.concurrent import thread_map
-
-
-def compute_size(dataset_path):
-    dataset = wds.WebDataset(dataset_path) \
-                .decode("torchrgb") \
-                .to_tuple("jpg;png", "json")
-    count = 0
-    for i, example in tqdm.tqdm(enumerate(dataset)):
-        count += 1
-    return count
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", "--dataset", help="dataset path", required=True)
 args = parser.parse_args()
 
 dataset_path_list = list(braceexpand.braceexpand(args.dataset))
-result = thread_map(compute_size, dataset_path_list, max_workers=20)
 
-print(">>>>> Total size:", sum(result), " <<<<<")
+result = 0
+for dataset_path in tqdm.tqdm(dataset_path_list):
+    tar = tarfile.open(dataset_path)
+    result += len(tar.getnames()) // 2  # tar have json and png files for each example
+
+print(">>>>> Total size:", result, " <<<<<")
