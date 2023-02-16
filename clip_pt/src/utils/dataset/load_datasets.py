6f790e394b750e21e0792051bce3e0aca2fffe27
Gabriel Oliveira <gabrielsantosrv@outlook.com>
2023-02-16 11:23:40 -0300

    setting dataset size to webdatasets

--- clip_pt/src/utils/dataset/load_datasets.py
+++ clip_pt/src/utils/dataset/load_datasets.py
@@ -1,3 +1,4 @@
+import json
 import random
 from typing import Tuple, Any
 
@@ -46,15 +47,27 @@ def format_batch(batch):
 def load_datasets(config, vision_processor, text_tokenizer) -> \
         Tuple[DataLoader[Any], DataLoader[Any]]:
 
+    """
+        previously computed dataset sizes. This is necessary because __len__ method in WebDataset
+        returns an inaccurate value, so we have to set it manually.
+        Reference: https://github.com/Lightning-AI/lightning/issues/10290
+    """
+    with open("datasets_size.json") as file:
+        datasets_sizes = json.load(file)
+
     print(">>>>> Train datasets:", [dataset['path'] for dataset in config.datasets.train])
     print(">>>>> Validation datasets:", [dataset['path'] for dataset in config.datasets.validation])
 
     train = []
+    train_size = 0
     for dataset in config.datasets.train:
+        train_size += datasets_sizes["train"][dataset['name']]
         train += list(braceexpand.braceexpand(dataset['path']))
 
     val = []
+    val_size = 0
     for dataset in config.datasets.validation:
+        val_size += datasets_sizes["validation"][dataset['name']]
         val += list(braceexpand.braceexpand(dataset['path']))
 
     max_length = config.model.text_padding_size
@@ -74,6 +87,10 @@ def load_datasets(config, vision_processor, text_tokenizer) -> \
                         .map(lambda x: tokenize(x, vision_processor, text_tokenizer, max_length)) \
                         .batched(config.batch_size) \
                         .map(format_batch)
+
+    # dataset size correctly
+    train_dataset = train_dataset.with_length(train_size)
+    val_dataset = val_dataset.with_length(val_size)
 
     train_dataloader = DataLoader(train_dataset, batch_size=None, num_workers=10)
     val_dataloader = DataLoader(val_dataset, batch_size=None, num_workers=10)
