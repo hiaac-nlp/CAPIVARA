1da74fd2f29cd0b1e93e22f9c34dfae729948fc8
Gabriel Oliveira <gabrielsantosrv@outlook.com>
2023-05-25 09:48:38 -0300

    refactoring self distill

--- clip_pt/src/models/self_distill_clip_wrapper.py
+++ clip_pt/src/models/self_distill_clip_wrapper.py
@@ -20,7 +20,7 @@ class SelfDistillCLIPWrapper(OpenCLIPWrapper):
         super().__init__(config, train_size=train_size, val_labels=val_labels, model=model,
                          carbon_tracker=carbon_tracker)
         self.mse = nn.MSELoss()
-        self.kl = nn.KLDivLoss()
+        self.kl = nn.KLDivLoss(log_target=True)
         if isinstance(config.alpha, DictConfig):
             self.alpha_constant = False
             self.max_alpha = config.alpha.max_alpha
@@ -36,52 +36,10 @@ class SelfDistillCLIPWrapper(OpenCLIPWrapper):
         optimizer = self.optimizers()
         lr_scheduler = self.lr_schedulers()
 
-        image_input, text_pt_input, text_en_input = train_batch
-
-        image_features, text_pt_features = self.model.encode((image_input, text_pt_input))
-        logits_per_image_pt, logits_per_text_pt = self.model.compute_logits(image_features,
-                                                                      text_pt_features,
-                                                                      fixed_logit=False)
-        contrastive_loss = clip_loss(logits_per_text_pt)
-
-        if self.alpha_constant:
-            alpha = self.alpha
-        else:
-            alpha = self.compute_alpha()
-
-        if self.config.self_distill == "kl":
-            with torch.no_grad():
-                text_en_features = self.model.encode_text(text_en_input)
-
-            _, logits_per_text_en = self.model.compute_logits(image_features,
-                                                              text_en_features,
-                                                              fixed_logit=False)
-
-            distillation_loss = self.kl(input=F.softmax(logits_per_text_pt / 3.0, dim=1),
-                                        target=F.softmax(logits_per_text_en / 3.0, dim=1))
-            mse_loss = self.mse(input=text_pt_features, target=text_en_features)
-
-            loss = alpha * contrastive_loss + (1 - alpha) * (distillation_loss + mse_loss)
-            self.log("train/KL pt-en", distillation_loss)
-            self.log("train/mse_loss", mse_loss)
-        else:
-            if self.config.self_distill == "complete":
-                text_en_features = self.model.encode_text(text_en_input)
-                _, logits_per_text_en = self.model.compute_logits(image_features,
-                                                                  text_en_features,
-                                                                  fixed_logit=False)
-                contrastive_loss_en = clip_loss(logits_per_text_en)
-                self.log("train/infoNCE_en", contrastive_loss_en)
-                self.log("train/infoNCE_pt", contrastive_loss)
-
-                contrastive_loss = (contrastive_loss_en + contrastive_loss) / 2
-            else:
-                with torch.no_grad():
-                    text_en_features = self.model.encode_text(text_en_input)
-
-            mse_loss = self.mse(input=text_pt_features, target=text_en_features)
-            loss = alpha * contrastive_loss + (1 - alpha) * mse_loss
-            self.log("train/mse_loss", mse_loss)
+        image_features, text_en_features, text_pt_features = self.extract_features(train_batch)
+        loss, logits_per_image_pt, logits_per_text_pt = self.compute_loss(image_features,
+                                                                          text_pt_features,
+                                                                          text_en_features)
 
         optimizer.zero_grad()
         self.manual_backward(loss)
@@ -98,12 +56,23 @@ class SelfDistillCLIPWrapper(OpenCLIPWrapper):
         batch_text_accuracy = self.text_train_acc(preds_text, ground_truth)
 
         self.log("train/loss", loss)
-        self.log("train/alpha", alpha)
-        self.log("train/infoNCE", contrastive_loss)
         self.log("train/batch_image_accuracy", batch_image_accuracy)
         self.log("train/batch_text_accuracy", batch_text_accuracy)
 
+    def extract_features(self, train_batch):
+        image_input, text_pt_input, text_en_input = train_batch
+        image_features, text_pt_features = self.model.encode((image_input, text_pt_input))
+        if self.config.self_distill == "complete":
+            text_en_features = self.model.encode_text(text_en_input)
+        else:
+            with torch.no_grad():
+                text_en_features = self.model.encode_text(text_en_input)
+        return image_features, text_en_features, text_pt_features
+
     def compute_alpha(self):
+        if self.alpha_constant:
+            return self.alpha
+
         if self.trainer.global_step <= self.warmup_start:
             return self.min_alpha
 
@@ -112,6 +81,44 @@ class SelfDistillCLIPWrapper(OpenCLIPWrapper):
                 (self.max_alpha - self.min_alpha) / (self.warmup_end - self.warmup_start)
 
         return self.max_alpha
+
+    def compute_loss(self, image_features, text_pt_features, text_en_features):
+        logits_per_image_pt, logits_per_text_pt = self.model.compute_logits(image_features,
+                                                                            text_pt_features,
+                                                                            fixed_logit=False)
+        contrastive_loss = clip_loss(logits_per_text_pt)
+
+        alpha = self.compute_alpha()
+
+        if self.config.self_distill == "kl":
+            _, logits_per_text_en = self.model.compute_logits(image_features,
+                                                              text_en_features,
+                                                              fixed_logit=False)
+
+            distillation_loss = self.kl(input=F.log_softmax(logits_per_text_pt / 3.0, dim=1),
+                                        target=F.log_softmax(logits_per_text_en / 3.0, dim=1))
+
+            loss = alpha * contrastive_loss + (1 - alpha) * distillation_loss
+            self.log("train/KL pt-en", distillation_loss)
+        else:
+            if self.config.self_distill == "complete":
+                _, logits_per_text_en = self.model.compute_logits(image_features,
+                                                                  text_en_features,
+                                                                  fixed_logit=False)
+                contrastive_loss_en = clip_loss(logits_per_text_en)
+                self.log("train/infoNCE_en", contrastive_loss_en)
+                self.log("train/infoNCE_pt", contrastive_loss)
+
+                contrastive_loss = (contrastive_loss_en + contrastive_loss) / 2
+
+            mse_loss = self.mse(input=text_pt_features, target=text_en_features)
+            loss = alpha * contrastive_loss + (1 - alpha) * mse_loss
+            self.log("train/mse_loss", mse_loss)
+
+        self.log("train/alpha", alpha)
+        self.log("train/infoNCE", contrastive_loss)
+
+        return loss
 
 
 class TeacherStudentSelfDistillCLIPWrapper(SelfDistillCLIPWrapper):
@@ -130,69 +137,15 @@ class TeacherStudentSelfDistillCLIPWrapper(SelfDistillCLIPWrapper):
             param.requires_grad = False
 
     def training_step(self, train_batch, batch_idx):
-        optimizer = self.optimizers()
-        lr_scheduler = self.lr_schedulers()
+        self.config.self_distill = self.config.self_distill.replace("teacher-", "")
+        super().training_step(train_batch, batch_idx)
 
+    def extract_features(self, train_batch):
         image_input, text_pt_input, text_en_input = train_batch
-
         image_features, text_pt_features = self.model.encode((image_input, text_pt_input))
-        logits_per_image_pt, logits_per_text_pt = self.model.compute_logits(image_features,
-                                                                      text_pt_features,
-                                                                      fixed_logit=False)
-        contrastive_loss = clip_loss(logits_per_text_pt)
-
-        if self.alpha_constant:
-            alpha = self.alpha
+        if self.config.self_distill == "complete":
+            text_en_features = self.teacher(text_en_input)
         else:
-            alpha = self.compute_alpha()
-
-        if self.config.self_distill == "teacher-kl":
             with torch.no_grad():
                 text_en_features = self.teacher(text_en_input)
-
-            _, logits_per_text_en = self.model.compute_logits(image_features,
-                                                              text_en_features,
-                                                              fixed_logit=False)
-
-            distillation_loss = self.kl(input=F.softmax(logits_per_text_pt / 3.0, dim=1),
-                                        target=F.softmax(logits_per_text_en / 3.0, dim=1))
-
-            loss = alpha * contrastive_loss + (1 - alpha) * distillation_loss
-            self.log("train/KL pt-en", distillation_loss)
-        else:
-            if self.config.self_distill == "teacher-complete":
-                text_en_features = self.teacher(text_en_input)
-                _, logits_per_text_en = self.model.compute_logits(image_features,
-                                                                  text_en_features,
-                                                                  fixed_logit=False)
-                contrastive_loss_en = clip_loss(logits_per_text_en)
-                self.log("train/infoNCE_en", contrastive_loss_en)
-                self.log("train/infoNCE_pt", contrastive_loss)
-
-                contrastive_loss = (contrastive_loss_en + contrastive_loss) / 2
-            else:
-                text_en_features = self.teacher(text_en_input)
-
-            mse_loss = self.mse(input=text_pt_features, target=text_en_features)
-            loss = alpha * contrastive_loss + (1 - alpha) * mse_loss
-            self.log("train/mse_loss", mse_loss)
-
-        optimizer.zero_grad()
-        self.manual_backward(loss)
-        optimizer.step()
-        if lr_scheduler:
-            lr_scheduler.step(loss)
-
-        preds_image = logits_per_image_pt.argmax(dim=1)
-        preds_text = logits_per_text_pt.argmax(dim=1)
-        ground_truth = torch.arange(len(logits_per_image_pt), dtype=torch.long,
-                                    device=logits_per_image_pt.device)
-
-        batch_image_accuracy = self.image_train_acc(preds_image, ground_truth)
-        batch_text_accuracy = self.text_train_acc(preds_text, ground_truth)
-
-        self.log("train/loss", loss)
-        self.log("train/alpha", alpha)
-        self.log("train/infoNCE", contrastive_loss)
-        self.log("train/batch_image_accuracy", batch_image_accuracy)
-        self.log("train/batch_text_accuracy", batch_text_accuracy)
+        return image_features, text_en_features, text_pt_features
